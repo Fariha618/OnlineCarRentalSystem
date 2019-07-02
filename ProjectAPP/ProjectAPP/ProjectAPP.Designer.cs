805de78a649338f56ecfad874a5996e6cc9c2e61
hasanscse <hasan.scse@gmail.com>
2019-07-02 15:30:36 +0600

    Button_Added

--- ProjectAPP/ProjectAPP/ProjectAPP.Designer.cs
+++ ProjectAPP/ProjectAPP/ProjectAPP.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(523, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // ProjectAPP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "ProjectAPP";
             this.Text = "Project App";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
